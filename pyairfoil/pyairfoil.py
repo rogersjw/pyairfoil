f8244f5e4c609b3f88e2fff7baa4881d3d05fe7f
rogersjw <jonathan.w.rogers@gmail.com>
2018-10-14 11:42:35 -0700

    Fixing Tox and travis configs

--- pyairfoil/pyairfoil.py
+++ pyairfoil/pyairfoil.py
@@ -9,9 +9,11 @@ AIRFOIL_TYPES = {}
 class AirfoilBase:
 
     def __init__(self, **kwargs):
-        # Register myself as an airfoil class type. Useful in the future when trying to re-load from a dictionary
+        # Register myself as an airfoil class type. Useful in the future when
+        # trying to re-load from a dictionary
         if self.__class__.__name__ not in AIRFOIL_TYPES.keys():
-            print('Registering {} as new Airfoil Type'.format(self.__class__.__name__))
+            print('Registering {} as new Airfoil Type'
+                  .format(self.__class__.__name__))
             AIRFOIL_TYPES[self.__class__.__name__] = self.__class__
         pass
 
