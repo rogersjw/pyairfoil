fdb314405a8e5fa6a385996a3aa69e1f3bb650f7
rogersjw <jonathan.w.rogers@gmail.com>
2018-10-14 12:15:50 -0700

    Fixed doc issue

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-import pyairfoil_package
+import pyairfoil
 
 # -- General configuration ---------------------------------------------
 
